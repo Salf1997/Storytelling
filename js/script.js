8bd18bbfbe64d0bc91143e273d6657c9e6986c63
Salf1997 <sofia.lopez_97@hotmail.com>
2019-01-27 18:44:54 +0100

    Tramo 3
    del 2000 al 2006

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ function llevame(identificacion){
 	var id = document.getElementById(identificacion);
 	id.scrollIntoView();
 }
+
+function siguiente(){
+	$.scrollify.next();
+}
+
+scrollify.next = function() {
+if(index<names.length) {
+  index += 1;
+  //index, instant, callbacks, toTop
+  animateScroll(index,false,true,true);
+}
+};
+scrollify.previous = function() {
+if(index>0) {
+  index -= 1;
+  //index, instant, callbacks, toTop
+  animateScroll(index,false,true,true);
+}
+};
